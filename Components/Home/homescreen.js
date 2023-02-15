369db50e0479d434ca7ec1918404c82b09b83fb7
donpeterjoseph <peterdon484@gmail.com>
2023-02-15 14:10:59 +0530

    homescreen icons changed

--- Components/Home/homescreen.js
+++ Components/Home/homescreen.js
@@ -18,34 +18,34 @@ const Home=({navigation})=>{
     return(
         <View style={styles.outline}>
             <View style={styles.navbar}>
-                <View>
+                <View style={{flex:1,borderColor:'red',borderWidth:2}}>
                     {/* <Text>nothing</Text> */}
                 </View>
                 <View style={styles.logo}>
-                   <Image style={styles.inputlogo} source={require('../assets/adaptiveicon.png')}/>
+                   <Image style={[styles.inputlogo,{borderWidth:3,borderColor:'red'}]} source={require('./assets/adaptiveicon.png')}/>
                 </View>
-                <Pressable>
-                    <Text>user</Text>
+                <Pressable style={styles.profile}>
+                    <Image style={{width:50,height:50,borderRadius:30}} source={require('./assets/scan.jpg')}/>
                 </Pressable>
             </View>
             <View style={styles.features}>
                 <Pressable style={styles.box}>
-                   <Image style={styles.inputlogo} source={require('../assets/scan.png')}/>
+                   <Image style={[{borderRadius:30},styles.featureimages]} source={require('./assets/scan.png')}/>
                     <Text>Scan</Text></Pressable>
                 <Pressable style={styles.box}>
-                   <Image style={styles.inputlogo} source={require('../assets/food.png')}/>
+                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/diet.jpg')}/>
                     <Text>Manage Diet</Text></Pressable>
                 <Pressable style={styles.box}>
-                   <Image style={styles.inputlogo} source={require('../assets/shop.png')}/>
+                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/cart.jpg')}/>
                     <Text>Medicine Shop</Text></Pressable>
                 <Pressable style={styles.box}>
-                   <Image style={styles.inputlogo} source={require('../assets/statistics.png')}/>
+                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/statistics.png')}/>
                     <Text>Statistics</Text></Pressable>
                 <Pressable style={styles.box}>
-                   <Image style={styles.inputlogo} source={require('../assets/doctor.png')}/>
+                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/doctor.jpg')}/>
                     <Text>Consult Doctor</Text></Pressable>
                 <Pressable style={styles.box}>
-                   <Image style={styles.inputlogo} source={require('../assets/customercare.png')}/>
+                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/customercare.jpg')}/>
                     <Text>Customer Care</Text></Pressable>
             </View>
         </View>
@@ -60,9 +60,9 @@ const styles=StyleSheet.create({
         height:windowHeight,
         justifyContent: 'flex-start',
         alignItems: 'flex-start',
-        backgroundColor:' #000000',
+        backgroundColor:'#E9E9E9',
         borderWidth:2,
-        borderColor:'black'
+        borderColor:'yellow'
     },
     content:{
         justifyContent:'center',
@@ -75,6 +75,7 @@ const styles=StyleSheet.create({
         paddingBottom:30,
         // borderWidth:1,
         // borderColor:'red',
+        alignItems:'center',
         width:'100%',
         justifyContent:'space-between'
     },
@@ -92,11 +93,17 @@ const styles=StyleSheet.create({
         // position:'absolute',
         width:'30%',
         height:'210%',
-        // borderColor:'blue',
-        // borderWidth:2,
+        borderColor:'blue',
+        borderWidth:2,
+        flex:6,
         alignItems:'center'
     },
     inputlogo:{
+        flex:1,
+        width:'40%',
+        height:'40%'
+    },
+    featureimages:{
         flex:1,
         width:'70%',
         height:'70%'
@@ -109,10 +116,18 @@ const styles=StyleSheet.create({
         height:'30%',
         justifyContent:'center',
         alignItems:'center',
-        backgroundColor:'#f5ffff',
+        // backgroundColor:'#f5ffff',
         borderRadius:10,
         // padding:20,
 
+    },
+    profile:{
+        flex:1,
+        borderWidth:2,
+        borderColor:'red',
+        borderRadius:40,
+        // height:50,
+        // width:40
     }
 })
 
