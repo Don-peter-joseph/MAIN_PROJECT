8f3d9d3cb75c49c135fd35d92af058959f55177c
donpeterjoseph <peterdon484@gmail.com>
2023-02-17 15:27:31 +0530

    some pages like diet,customer care etc.. added

--- Components/Home/homescreen.js
+++ Components/Home/homescreen.js
@@ -11,41 +11,89 @@ const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 
-const Home=({navigation})=>{
-    
+const Home=({navigation,route})=>{
+    const [a,a1] = useState(false);
+    const [b,b1] = useState(false);
+    const [c,c1] = useState(false);
+    const [d,d1] = useState(false);
+    const [e,e1] = useState(false);
+    const [f,f1] = useState(false);
+
+    // const handlePressIn = () => {
+    //     a1(true);
+    //   };
+    //   const handlePressOut = () => {
+    //     a1(false);
+    //   };
+
+    //   const handlePressIn2 = () => {
+    //     b1(true);
+    //   };
+    //   const handlePressOut2 = () => {
+    //     b1(false);
+    //   };
+
+    //   const handlePressIn3 = () => {
+    //     c1(true);
+    //   };
+    //   const handlePressOut3 = () => {
+    //     c1(false);
+    //   };
+
+    //   const handlePressIn4 = () => {
+    //     d1(true);
+    //   };
+    //   const handlePressOut4 = () => {
+    //     d1(false);
+    //   };
+
+    //   const handlePressIn5 = () => {
+    //     e1(true);
+    //   };
+    //   const handlePressOut5 = () => {
+    //     e1(false);
+    //   };
+
+    //   const handlePressIn6 = () => {
+    //     f1(true);
+    //   };
+    //   const handlePressOut6 = () => {
+    //     f1(false);
+    //   };
+
     const redirectUrl = Linking.createURL('home');
       console.log(redirectUrl);
     return(
         <View style={styles.outline}>
             <View style={styles.navbar}>
-                <View style={{flex:1,borderColor:'red',borderWidth:2}}>
+                <View style={{flex:1}}>
                     {/* <Text>nothing</Text> */}
                 </View>
                 <View style={styles.logo}>
-                   <Image style={[styles.inputlogo,{borderWidth:3,borderColor:'red'}]} source={require('./assets/adaptiveicon.png')}/>
+                   <Image style={[styles.inputlogo]} source={require('./assets/adaptiveicon.png')}/>
                 </View>
                 <Pressable style={styles.profile}>
                     <Image style={{width:50,height:50,borderRadius:30}} source={require('./assets/scan.jpg')}/>
                 </Pressable>
             </View>
             <View style={styles.features}>
-                <Pressable style={styles.box}>
-                   <Image style={[{borderRadius:30},styles.featureimages]} source={require('./assets/scan.png')}/>
+                <Pressable  style={styles.box} onPress={()=>navigation.navigate("scanscreen")}>
+                   <Image style={[styles.featureimages]} source={require('./assets/scan.png')}/>
                     <Text>Scan</Text></Pressable>
-                <Pressable style={styles.box}>
-                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/diet.jpg')}/>
+                <Pressable  style={styles.box} onPress={()=>navigation.navigate("dietscreen")}>        
+                   <Image style={[styles.featureimages]} source={require('./assets/diet.jpg')}/>
                     <Text>Manage Diet</Text></Pressable>
-                <Pressable style={styles.box}>
-                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/cart.jpg')}/>
+                <Pressable style={styles.box} onPress={()=>navigation.navigate("shopscreen")}>
+                   <Image style={[styles.featureimages]} source={require('./assets/cart.jpg')}/>
                     <Text>Medicine Shop</Text></Pressable>
-                <Pressable style={styles.box}>
-                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/statistics.png')}/>
+                <Pressable style={styles.box} onPress={()=>navigation.navigate("statisticsscreen")}>
+                   <Image style={[styles.featureimages]} source={require('./assets/statistics.png')}/>
                     <Text>Statistics</Text></Pressable>
-                <Pressable style={styles.box}>
-                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/doctor.jpg')}/>
+                <Pressable style={styles.box} onPress={()=>navigation.navigate("consultscreen")}>
+                   <Image style={[styles.featureimages]} source={require('./assets/doctor.jpg')}/>
                     <Text>Consult Doctor</Text></Pressable>
-                <Pressable style={styles.box}>
-                   <Image style={[styles.featureimages,{borderRadius:30}]} source={require('./assets/customercare.jpg')}/>
+                <Pressable style={styles.box} onPress={()=>navigation.navigate("servicescreen")}>
+                   <Image style={[styles.featureimages]} source={require('./assets/customercare.jpg')}/>
                     <Text>Customer Care</Text></Pressable>
             </View>
         </View>
@@ -61,8 +109,8 @@ const styles=StyleSheet.create({
         justifyContent: 'flex-start',
         alignItems: 'flex-start',
         backgroundColor:'#E9E9E9',
-        borderWidth:2,
-        borderColor:'yellow'
+        // borderWidth:2,
+        // borderColor:'yellow'
     },
     content:{
         justifyContent:'center',
@@ -80,8 +128,8 @@ const styles=StyleSheet.create({
         justifyContent:'space-between'
     },
     features:{
-        borderWidth:2,
-        borderColor:'green',
+        // borderWidth:2,
+        // borderColor:'green',
         width:'100%',
         height:'50%',
         flexDirection:'row',
@@ -93,8 +141,8 @@ const styles=StyleSheet.create({
         // position:'absolute',
         width:'30%',
         height:'210%',
-        borderColor:'blue',
-        borderWidth:2,
+        // borderColor:'blue',
+        // borderWidth:2,
         flex:6,
         alignItems:'center'
     },
@@ -106,12 +154,14 @@ const styles=StyleSheet.create({
     featureimages:{
         flex:1,
         width:'70%',
-        height:'70%'
+        height:'70%',
+        borderRadius:30,
     },
     box:{
         // borderColor:'red',
         // borderWidth:1,
-        width:'30%',
+        width:'33%',
+        padding:5,
         marginTop:30,
         height:'30%',
         justifyContent:'center',
@@ -121,10 +171,13 @@ const styles=StyleSheet.create({
         // padding:20,
 
     },
+    boxPressed:{
+        padding:2,
+    },
     profile:{
         flex:1,
-        borderWidth:2,
-        borderColor:'red',
+        // borderWidth:2,
+        // borderColor:'red',
         borderRadius:40,
         // height:50,
         // width:40
---
+++ Components/Home/statistics.js
@@ -0,0 +1,50 @@
+import { Auth,Hub } from "aws-amplify";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, TextInput,Pressable,Image,ImageBackground, Alert } from "react-native";
+import {Dimensions} from 'react-native';
+import * as Linking from 'expo-linking';
+import * as webbrower from 'expo-web-browser';
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const Statistics=({navigation,route})=>{
+
+    return(
+        <View style={styles.outline}>
+            <View style={styles.content}>
+                <Text>This is scannig screen</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    outline:{
+        flex: 1,
+        position:'absolute',
+        width:windowWidth,
+        height:windowHeight,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:' #f5f5f5'
+    },
+    content:{
+        justifyContent:'center',
+        alignItems:"center"
+    }   ,
+    signout:{
+        borderWidth:1,
+        borderColor:'black',
+        borderRadius:25,
+        backgroundColor:'#8A8AFF',
+        height:'13%',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:13,
+        width:'50%'
+    }
+})
+
+export default Statistics;
