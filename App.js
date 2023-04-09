a5030c525ec10a9156528cb891a1defc93b70f97
Amith Bino <amith1062@gmail.com>
2023-04-09 11:11:10 +0530

    changes

--- App.js
+++ App.js
@@ -118,12 +118,12 @@ export default function App() {
         </>
          : 
          ( <>
-            <Stack.Screen name="signin" component={Signin}/>
-            <Stack.Screen name="signup" component={Signup}/>
             <Stack.Screen name="detailsScreen" component={Detailsfirst}/>
-            <Stack.Screen name="details2screen" component={Detailssecond}/>
-            <Stack.Screen name="details3screen" component={Detailsthird}/>                                                                        " component={Signin}/>
+            <Stack.Screen name="signin" component={Signin}/>
             <Stack.Screen name="homescreen" component={Home}/>
+            <Stack.Screen name="signup" component={Signup}/>
+            <Stack.Screen name="details2screen" component={Detailssecond}/>
+            <Stack.Screen name="details3screen" component={Detailsthird}/>                                                           
             <Stack.Screen name="profilescreen" component={Profile}/> 
             <Stack.Screen name="confirmemail" component={Confirmemail}/>
             <Stack.Screen name="forgotpassword" component={Forgotpassword}/>
